adf95fc7ea440b2fe5bbcf4e858f1872ae54f1d0
Black Diamond <6.black.diamond.9@gmail.com>
2022-02-19 00:35:19 +0200

    Added zombie and its AI script
    Add the Enemy scene including a Zombie script that handle the basic AI of tracking the player and moving towards him

---
+++ Scripts/Zombie.gd
@@ -0,0 +1,49 @@
+extends KinematicBody
+
+export var speed:float = 15
+export var acceleration:float = 7.5
+export var gravity:float = 0.98
+export var max_terminal_vel:float = 54
+
+var space_state:PhysicsDirectSpaceState
+var path_index
+var path = []
+var navigation:Navigation
+var target:Vector3 = Vector3.ZERO
+
+var velocity:Vector3
+var y_velocity:float
+
+func _ready():
+	space_state = get_world().direct_space_state
+	navigation = get_parent()
+	if navigation:
+		print("found navigation")
+	else:
+		print("cant find navigation")
+
+func _process(delta):
+	var player:KinematicBody = get_parent().get_parent().get_node("Player");
+	if player:
+		target = player.global_transform.origin
+
+func _physics_process(delta):
+	handle_movement(delta)
+
+func handle_movement(delta):
+	var direction:Vector3 = Vector3.ZERO
+	if path and path_index < path.size():
+		direction = path[path_index] - global_transform.origin
+		if direction.length() < 1:
+			path_index += 1
+		else:
+			direction = direction.normalized()
+			velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+			velocity = move_and_slide(velocity,Vector3.UP)
+
+func _on_Timer_timeout():
+	get_target(target)
+
+func get_target(pos):
+	path = navigation.get_simple_path(global_transform.origin,pos)
+	path_index = 0
