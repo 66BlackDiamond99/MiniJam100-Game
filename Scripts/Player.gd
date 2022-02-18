1e9b1c5608c4faa497c0771792fe40378cbdc39c
Black Diamond <6.black.diamond.9@gmail.com>
2022-02-18 22:47:59 +0200

    Add movement to player script
    Add the movement control of the player and instanced the player scene into the main scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,16 +1,66 @@
 extends KinematicBody
 
+export var speed:float = 20
+export var acceleration:float = 15
+export var air_acceleration:float = 5
+export var gravity:float = 0.98
+export var max_terminal_vel:float = 54
+export var jump_power:float = 20
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(float,0.1,1) var mouse_sensitivity:float = 0.3
+export(float,-90,0) var min_pitch:float = -90
+export(float,0,90) var max_pitch:float = 90
 
+var velocity:Vector3
+var y_velocity:float
 
-# Called when the node enters the scene tree for the first time.
+onready var camera_pivot = $CameraPivot
+onready var camera = $CameraPivot/CameraHolder/Camera
+
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if event is InputEventMouseMotion:
+		event = event as InputEventMouseMotion
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		camera_pivot.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		camera_pivot.rotation_degrees.x = clamp(camera_pivot.rotation_degrees.x, min_pitch,max_pitch)
+
+func _physics_process(delta):
+	handle_movement(delta)
+
+func handle_movement(delta):
+	var direction:Vector3 = Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		direction -= transform.basis.z
+	
+	if Input.is_action_pressed("move_backward"):
+		direction += transform.basis.z
+	
+	if Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
+	
+	if Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x
+	
+	direction = direction.normalized()
+	
+	var acc = acceleration if is_on_floor() else air_acceleration
+	velocity = velocity.linear_interpolate(direction * speed, acc * delta)
+	
+	if is_on_floor():
+		y_velocity = -0.1
+	else:
+		y_velocity = clamp(y_velocity - gravity, -max_terminal_vel, max_terminal_vel)
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		y_velocity = jump_power
+	
+	velocity.y = y_velocity
+	velocity = move_and_slide(velocity,Vector3.UP)
+	
